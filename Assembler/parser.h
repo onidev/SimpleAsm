1561b1367a4f87d058231177aacf3fa08b19936f
Boris M <onilink_@hotmail.fr>
2015-01-06 12:15:19 +0100

    Create parser.h

---
+++ Assembler/parser.h
@@ -0,0 +1,12 @@
+#ifndef PARSER_H_INCLUDED
+#define PARSER_H_INCLUDED
+
+#include <string>
+
+class Parser
+{
+    public:
+    void parseFile(const std::string & in, const std::string & out);
+};
+
+#endif // PARSER_H_INCLUDED
